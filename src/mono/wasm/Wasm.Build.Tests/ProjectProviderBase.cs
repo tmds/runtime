b893f8718e0059181a89a17430abdc988322ea55
Ankit Jain <radical@gmail.com>
2024-01-09 16:01:49 -0500

    [wasm] Fix checking for icudt files against the runtime pack
    The files in the appbundle were being compared against the file copied
    to the bindir for Wasm?Build.Tests, like
    `artifacts/bin/Wasm.Build.Tests/Release/net8.0/browser-wasm`.
    
    But since WBT is not being built to target wasm anymore, these files are
    never copied to the bindir. Instead, construct the path to runtime pack
    and check against that. This is the correct behavior.

--- src/mono/wasm/Wasm.Build.Tests/ProjectProviderBase.cs
+++ src/mono/wasm/Wasm.Build.Tests/ProjectProviderBase.cs
@@ -385,7 +385,13 @@ public abstract class ProjectProviderBase(ITestOutputHelper _testOutput, string?
             actual = actual.Union(Directory.EnumerateFiles(assertOptions.BinFrameworkDir, "segmentation-rules.json"));
         AssertFilesOnDisk(expected, actual);
         if (assertOptions.GlobalizationMode is GlobalizationMode.PredefinedIcu)
-            TestUtils.AssertSameFile(assertOptions.PredefinedIcudt!, actual.Single());
+        {
+            string srcPath = assertOptions.PredefinedIcudt!;
+            string runtimePackDir = BuildTestBase.s_buildEnv.GetRuntimeNativeDir(assertOptions.TargetFramework, assertOptions.RuntimeType);
+            if (!Path.IsPathRooted(srcPath))
+                srcPath = Path.Combine(runtimePackDir, assertOptions.PredefinedIcudt!);
+            TestUtils.AssertSameFile(srcPath, actual.Single());
+        }
     }
 
     public void AssertBootJson(AssertBundleOptionsBase options)
